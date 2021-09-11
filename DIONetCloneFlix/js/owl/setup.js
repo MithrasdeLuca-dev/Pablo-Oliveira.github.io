ef6ce190f69734e727797f5ffb5507fef7711457
Pablo Oliveira <pablue@gmail.com>
2021-09-10 21:13:05 -0400

    Configuração incial

---
+++ DIONetCloneFlix/js/owl/setup.js
@@ -0,0 +1,33 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+
+//meu código
+window.onload = function () {
+
+    // Video
+    var video = document.getElementById("video");
+
+    // Buttons
+    var playButton = document.getElementById("play-pause");
+    var muteButton = document.getElementById("mute");
+    var fullScreenButton = document.getElementById("full-screen");
+
+    // Sliders
+    var seekBar = document.getElementById("seek-bar");
+    var volumeBar = document.getElementById("volume-bar");
+
+}
